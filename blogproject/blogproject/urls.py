d6155a8676d54ff0271b50d55b2ce78a613bf695
Himanshu Kumar <himanshukumar51667@gmail.com>
2026-07-01 00:25:34 +0530

    Add user registration view and template

--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
-
+from users import views as user_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("blog/",include('blog.urls')),  # add app in django then include
+    path("register",user_views.register,name='register'),
 
 ]
